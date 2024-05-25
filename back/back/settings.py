b9a9705d8d8ee78dea24b0530363aacb65da264c
Risson <aime.risson.1@gmail.com>
2024-05-25 14:40:08 +0200

    pagination

--- back/back/settings.py
+++ back/back/settings.py
@@ -61,7 +61,11 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny'
     ],
+    "PAGE_SIZE": 20,
+    "MAX_PAGINATE_BY": 100,
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
 }
 
 SPECTACULAR_SETTINGS = {
--- back/news_feed/serializers.py
+++ back/news_feed/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import News
+
 class NewsSerializer(serializers.ModelSerializer):
     class Meta:
         model = News
